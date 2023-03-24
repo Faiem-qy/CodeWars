8d808cfa03fa2199b91a3c53a12635b7d70d8106
Faiem-qy <f.quanyoung@gmail.com>
2023-03-23 23:13:23 -0400

    cw

---
+++ cw.js
@@ -0,0 +1,27 @@
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString();
+
+}
+
+// or 
+
+function numberToString(num) {
+  return '' + num;
+}
+
+
+// or
+
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+}
+
+// or 
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return `${num}`;
+}
