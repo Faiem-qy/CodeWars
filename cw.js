accece8e6bcd2935ce1008732ebfd1d3208c6164
Faiem-qy <f.quanyoung@gmail.com>
2023-04-04 22:17:33 -0400

    cw

--- cw.js
+++ cw.js
@@ -25,3 +25,20 @@ function numberToString(num) {
   // Return a string of the number here!
   return `${num}`;
 }
+
+
+var planetMoons = {
+  mercury: 0,
+  venus: 0,
+  earth: 1,
+  mars: 2,
+  jupiter: 67,
+  saturn: 62,
+  uranus: 27,
+  neptune: 14
+};
+
+for (var planet in planetMoons) {
+  var numberOfMoons = planetMoons[planet];
+  console.log("Planet: " + planet + ", # of Moons: " + numberOfMoons);
+}
