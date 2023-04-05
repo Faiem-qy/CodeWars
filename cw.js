27de989acbf86418ef51416aee19d88b3a6ef692
Faiem-qy <f.quanyoung@gmail.com>
2023-04-04 22:26:57 -0400

    cw

--- cw.js
+++ cw.js
@@ -42,3 +42,24 @@ for (var planet in planetMoons) {
   var numberOfMoons = planetMoons[planet];
   console.log("Planet: " + planet + ", # of Moons: " + numberOfMoons);
 }
+
+//Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+
+
+
+function disemvowel(str) {
+  // Define the regex pattern to match vowels (including capital vowels)
+  const vowels = /[aeiouAEIOU]/g;
+
+  // Use the replace method to remove vowels from the string and return the updated string
+  return str.replace(vowels, "");
+}
