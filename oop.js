71578faedfc59c23321fac4ba54da92772bf9368
Faiem-qy <f.quanyoung@gmail.com>
2023-04-10 18:36:59 -0400

    oop

--- oop.js
+++ oop.js
@@ -1,8 +1,57 @@
 
-// let pizza1 = new Pizza();
-// let pizza2 = new Pizza();
+// // let pizza1 = new Pizza();
+// // let pizza2 = new Pizza();
 
-// pizza1 === pizza2; // false
+// // pizza1 === pizza2; // false
+
+// class Pizza {
+
+//   constructor(size, crust) {
+//     this.size = size;
+//     this.crust = crust;
+//     this.toppings = ["cheese"];
+//     this.sauce = ["garlic"];
+//     this.seasoning = ["italian"];
+//   }
+
+//   addTopping(topping) {
+//     this.toppings.push(topping);
+//   }
+
+
+// }
+
+// // adding a method to a class using the followinf syntax
+// // class SomeClass {
+// //   methodName(parameters) {
+// //     // this is a method
+// //   }
+// // }
+
+// //adding properties to a class using the this. keyword
+// // class SomeClass {
+// //   someMethod() {
+// //     this.hello = "hi"; // Created a property called hello
+// //   }
+// // }
+
+
+// let pizza1 = new Pizza();
+// // console.log(pizza1.toppings);
+// pizza1.addTopping("mushrooms");
+// pizza1.addTopping("peppers");
+// // console.log(pizza1.toppings);
+
+// let pizza2 = new Pizza();
+// console.log(pizza2.toppings);
+// pizza2.addTopping("more cheese");
+// console.log(pizza2.toppings);
+// console.log(pizza2.sauce);
+// console.log(pizza2.sauce);
+
+
+// let pizza = new Pizza('large', 'thin');
+// console.log(pizza.toppings);
 
 class Pizza {
 
@@ -11,7 +60,7 @@ class Pizza {
     this.crust = crust;
     this.toppings = ["cheese"];
     this.sauce = ["garlic"];
-
+    this.seasoning = ["italian"];
   }
 
   addTopping(topping) {
@@ -22,37 +71,24 @@ class Pizza {
     this.sauce.push(sauce);
   }
 
+  addSeasoning(seasoning) {
+    this.seasoning.push(seasoning);
+  }
 }
 
-// adding a method to a class using the followinf syntax
-// class SomeClass {
-//   methodName(parameters) {
-//     // this is a method
-//   }
-// }
-
-//adding properties to a class using the this. keyword
-// class SomeClass {
-//   someMethod() {
-//     this.hello = "hi"; // Created a property called hello
-//   }
-// }
-
-
 let pizza1 = new Pizza();
-// console.log(pizza1.toppings);
+console.log(pizza1.toppings); // ["cheese"]
 pizza1.addTopping("mushrooms");
 pizza1.addTopping("peppers");
-// console.log(pizza1.toppings);
+console.log(pizza1.toppings); // ["cheese", "mushrooms", "peppers"]
+pizza1.addSauce("tomato");
+pizza1.addSauce("bbq");
+pizza1.addSeasoning('chili powder');
+console.log(pizza1.seasoning);
+console.log(pizza1.sauce);
 
 let pizza2 = new Pizza();
-console.log(pizza2.toppings);
+console.log(pizza2.toppings); // ["cheese"]
 pizza2.addTopping("more cheese");
-console.log(pizza2.toppings);
-console.log(pizza2.sauce);
-pizza2.addSauce("pesto");
-console.log(pizza2.sauce);
+console.log(pizza2.toppings); // ["cheese", "more cheese"];
 
-
-let pizza = new Pizza('large', 'thin');
-console.log(pizza.toppings);
