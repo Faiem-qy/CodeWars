b559a5aafcaeb81340eccbcbf49cb4ce938c4c85
Faiem-qy <f.quanyoung@gmail.com>
2023-04-19 23:00:41 -0400

    cw

--- cw.js
+++ cw.js
@@ -82,7 +82,7 @@
 // Dog Years
 // 15 dog years for first year
 // +9 dog years for second year
-// +5 dog years for each year after that 
+// +5 dog years for each year after that
 
 // function calculatePetAge(humanYears) {
 //   let catYears = 0;
@@ -105,11 +105,35 @@
 // }
 
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
-var countSheep = function(num) {
-  //your code here
-  let result = '';
-  for (let i = 1; i <= num; i++) {
-    result += i + ' sheep...';
+// var countSheep = function(num) {
+//   //your code here
+//   let result = '';
+//   for (let i = 1; i <= num; i++) {
+//     result += i + ' sheep...';
+//   }
+//   return result;
+// };
+/*
+There is a bus moving in the city which takes and drops some people at each bus stop.
+
+You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+Take a look on the test cases.
+
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+*/
+
+var number = function(busStops) {
+  // Good Luck!
+  let totalPeople = 0;
+  for (let i = 0; i < busStops.length; i++) {
+    totalPeople += busStops[i][0];
+    totalPeople -= busStops[i][1];
+
   }
-  return result;
+  return totalPeople >= 0 ? totalPeople : 0;
 };
