8a36c4343dd8dbd623bbc33e72dbc026cb0b2a66
Faiem-qy <f.quanyoung@gmail.com>
2023-05-12 22:28:46 -0400

    practice

--- cw.js
+++ cw.js
@@ -259,3 +259,20 @@ function digitize(n) {
   digits.reverse();
   return digits.map(Number);
 }
+
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty. 
+
+function getAverage(marks) {
+  // Calculate the sum of the marks
+  const sum = marks.reduce((acc, curr) => acc + curr, 0);
+
+  // Calculate the average
+  const avg = sum / marks.length;
+
+  // Round down to the nearest integer using Math.floor()
+  return Math.floor(avg);
+}
