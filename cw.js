bca72c1e6f1b6f1a1c6f74623a726636e8854bba
Faiem-qy <f.quanyoung@gmail.com>
2023-05-15 21:37:43 -0400

    practice

--- cw.js
+++ cw.js
@@ -294,3 +294,16 @@ function duplicates(arr) {
 
   return duplicates;
 }
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+
+function count(string) {
+  let result = {};
+
+  for (const char of string) {
+    result[char] = (result[char] || 0) + 1;
+  }
+  return result;
+}
