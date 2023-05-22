09118b2d9db4f8a4031c3c5d4ce1ad4a0b32cd0a
Faiem-qy <f.quanyoung@gmail.com>
2023-05-21 22:23:22 -0400

    practice

--- cw.js
+++ cw.js
@@ -346,3 +346,17 @@ function findAverage(array) {
 
   return sum / array.length;
 }
+
+function findAverage(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  var average = sum / array.length;
+  return average;
+}
