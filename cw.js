fb89114ea46aee85b30e23c9508e09a8f60f9b13
Faiem-qy <f.quanyoung@gmail.com>
2023-05-24 21:36:53 -0400

    practice

--- cw.js
+++ cw.js
@@ -382,3 +382,22 @@ function greet() {
 
   return `${message} ${world}${punctuation}`;
 }
+
+
+function getGrade(s1, s2, s3) {
+  // Calculate the average score
+  const average = (s1 + s2 + s3) / 3;
+
+  // Assign the letter grade based on the average score
+  if (average >= 90 && average <= 100) {
+    return "A";
+  } else if (average >= 80 && average < 90) {
+    return "B";
+  } else if (average >= 70 && average < 80) {
+    return "C";
+  } else if (average >= 60 && average < 70) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
