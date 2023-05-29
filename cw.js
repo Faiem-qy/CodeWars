62572fa3e56711c1402bbfac9b7f2012bb89a7fd
Faiem-qy <f.quanyoung@gmail.com>
2023-05-28 20:37:07 -0400

    practice

--- cw.js
+++ cw.js
@@ -439,3 +439,18 @@ const rps = (p1, p2) => {
   return "Player 2 won!";
 };
 
+
+function bmi(weight, height) {
+  const bmiValue = weight / (height * height);
+
+  if (bmiValue <= 18.5) {
+    return "Underweight";
+  } else if (bmiValue <= 25.0) {
+    return "Normal";
+  } else if (bmiValue <= 30.0) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
+
