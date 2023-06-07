6341b0830d87a36e42f416cb2f49bb8f0d46a6d6
Faiem-qy <f.quanyoung@gmail.com>
2023-06-06 21:21:29 -0400

    practice

--- cw.js
+++ cw.js
@@ -555,3 +555,13 @@ function betterThanAverage(classPoints, yourPoints) {
     return false;
   }
 }
+
+function getRealFloor(n) {
+  if (n <= 0) {
+    return n;
+  } else if (n <= 13) {
+    return n - 1;
+  } else {
+    return n - 2;
+  }
+}
