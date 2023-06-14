14b19ebeb657c2a4e16110f18da545cfc2461958
Faiem-qy <f.quanyoung@gmail.com>
2023-06-14 19:40:08 -0400

    practice

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ const sayHelloTo = require('./myModule');
 console.log('sayHelloTo: ', sayHelloTo);
 
 sayHelloTo('Bernie');
+
+function chromosomeCheck(s) {
+  if (s === 'X') {
+    return "Congratulations! You're going to have a daughter.";
+  } else if (s === 'Y') {
+    return "Congratulations! You're going to have a son.";
+  }
+}
