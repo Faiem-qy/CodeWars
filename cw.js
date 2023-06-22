cb9c6fd5b2ec6202bcaaadc5e6442839e0c67fea
Faiem-qy <f.quanyoung@gmail.com>
2023-06-21 20:55:28 -0400

    practice

--- cw.js
+++ cw.js
@@ -646,3 +646,13 @@ function remove(string) {
   }
   return string;
 }
+
+function findMultiples(integer, limit) {
+  let multiples = [];
+
+  for (let i = integer; i <= limit; i += integer) {
+    multiples.push(i);
+  }
+
+  return multiples;
+}
