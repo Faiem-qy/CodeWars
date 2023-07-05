73ac993fc3a292b8ee31ae53cad1856fc9d7b1b9
Faiem-qy <f.quanyoung@gmail.com>
2023-07-05 17:22:23 -0400

    practice

--- cw.js
+++ cw.js
@@ -737,3 +737,15 @@ const sequenceSum = (begin, end, step) => {
 
   return sum;
 };
+
+const multiplicationTable = function(size) {
+  const table = [];
+  for (let i = 1; i <= size; i++) {
+    const row = [];
+    for (let j = 1; j <= size; j++) {
+      row.push(i * j);
+    }
+    table.push(row);
+  }
+  return table;
+};
