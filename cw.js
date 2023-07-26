f94caadc22c59cf22a40183a3c228c3285060db6
Faiem-qy <f.quanyoung@gmail.com>
2023-07-25 23:00:19 -0400

    practice

--- cw.js
+++ cw.js
@@ -944,3 +944,16 @@ function distinct(a) {
 
   return newArray;
 }
+
+
+function drawStairs(n) {
+  let result = '';
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      result += ' ';
+  }
+  result += 'I\n';
+}
+console.log(result);
+}
+drawStairs(20);
