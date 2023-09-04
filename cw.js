0559cc983446f48800935a2fe00d71671434afeb
Faiem-qy <f.quanyoung@gmail.com>
2023-09-03 20:52:47 -0400

    practice

--- cw.js
+++ cw.js
@@ -1306,3 +1306,15 @@ function repeatStr(n, s) {
     return s.repeat(n); // Use the repeat method to repeat the string s n times
   }
 }
+
+
+function chromosomeCheck(sperm) {
+  if (sperm === 'XX') {
+    return "Congratulations! You're going to have a daughter.";
+  } else if (sperm === 'XY') {
+    return "Congratulations! You're going to have a son.";
+  } else {
+    // Handle invalid input (e.g., if sperm is neither 'X' nor 'Y')
+    return "Invalid input. Please provide 'X' or 'Y' for sperm.";
+  }
+}
