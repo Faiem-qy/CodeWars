cce22b623c1a4fc2db89787936963ca18be02ab8
Faiem-qy <f.quanyoung@gmail.com>
2023-11-14 18:35:41 -0500

    practice

--- cw.js
+++ cw.js
@@ -2013,7 +2013,7 @@ function minMax(arr) {
 
 const inputArray = [3, 1, 9, 5, 7];
 const result = minMax(inputArray);
-console.log(result); 
+console.log(result);
 
 
 function isPalindrome(str) {
@@ -2030,15 +2030,41 @@ function isPalindrome(str) {
 const testString = "A man, a plan, a canal, Panama!";
 const resultt = isPalindrome(testString);
 
-console.log(resultt)
+console.log(resultt);
 
 function stray(numbers) {
   let result = 0;
 
-  for (let i = 0; i < numbers.length; i ++) {
-    result ^= numbers[i]
+  for (let i = 0; i < numbers.length; i++) {
+    result ^= numbers[i];
   }
-  return result
+  return result;
 }
 
-console.log(stray([1,1,2,1,1]));
+console.log(stray([1, 1, 2, 1, 1]));
+
+
+function findNeedle(haystack) {
+  // your code here
+  const index = haystack.indexOf("needle");
+  if (index !== -1) {
+    return `found the needle at position ${index}`;
+  } else {
+    return "needle not found";
+  }
+}
+
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+
+
+
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])); // --> "found the needle at position 5" 
+
+function anyArrows(arrows) {
+  return arrows.some(arrow => !arrow.damaged);
+}
+
+console.log(anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}]));
