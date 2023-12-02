20c72cef07eb333b1e3abdcfd0c3a1283e5e7b81
Faiem-qy <f.quanyoung@gmail.com>
2023-12-01 19:31:15 -0500

    practice

--- cw.js
+++ cw.js
@@ -2154,3 +2154,13 @@ const flip = (d, a) => {
   return d === 'R' ? a.sort((x, y) => x - y) : a.sort((x, y) => y - x);
 }
 
+/*Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.*/
+
+const stringToNumber = function(str){
+  return parseInt(str, 10);
+}
