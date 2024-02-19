27759af9cdd7d1bd57401e3de94b413410c253ff
Faiem-qy <f.quanyoung@gmail.com>
2024-02-19 14:18:03 -0500

    practice

--- cw.js
+++ cw.js
@@ -2749,3 +2749,35 @@ function decrypt(encryptedText, n) {
   return encryptedText;
 }
 console.log(encrypt("012345", 1));  // Output: "135024"
+
+
+function switchItUp(number) {
+  //Write your own Code!
+  switch (number) {
+    case 0:
+      return "Zero";
+    case 1:
+      return "One";
+    case 2:
+      return "Two";
+    case 3:
+      return "Three";
+    case 4:
+      return "Four";
+    case 5:
+      return "Five";
+    case 6:
+      return "Six";
+    case 7:
+      return "Seven";
+    case 8:
+      return "Eight";
+    case 9:
+      return "Nine";
+    default:
+      return "Invalid number";
+  }
+}
+
+console.log(switchItUp(1));
+console.log(switchItUp(3));
