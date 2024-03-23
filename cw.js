df39870e96fd71bbaa9958dbb1ddeb3386f8ddad
Faiem-qy <f.quanyoung@gmail.com>
2024-03-23 14:29:06 -0400

    practice

--- cw.js
+++ cw.js
@@ -3208,3 +3208,11 @@ function addFive(num) {
   return total
 }
 
+function firstNonConsecutive(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1] + 1) {
+      return arr[i];
+    }
+  }
+  return null;
+}
