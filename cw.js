41b0fe13f7d4960ad8a3e5d55f306b98a571b789
Faiem-qy <f.quanyoung@gmail.com>
2024-04-30 18:24:00 -0400

    practice

--- cw.js
+++ cw.js
@@ -3668,3 +3668,18 @@ function hello(name) {
     return `Hello, World!`;
   }
 }
+
+
+function strCount(str, letter) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(strCount("Hello", 'o')); 
+console.log(strCount("Hello", 'l')); 
+console.log(strCount("", 'z'));
