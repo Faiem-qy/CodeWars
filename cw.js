08789ed2df39a94927702ed44cf96cc228440d0e
Faiem-qy <f.quanyoung@gmail.com>
2024-05-03 20:07:29 -0400

    practice

--- cw.js
+++ cw.js
@@ -3692,7 +3692,24 @@ console.log(correct("510"));
 
 function plural(n) {
   // ...
-return n !== 1;
+  return n !== 1;
 }
 
-plural(5)
+plural(5);
+
+
+
+function bmi(weight, height) {
+  let bmi = weight / (height * height);
+
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi <= 25.0) {
+    return "Normal";
+  } else if (bmi <= 30.0) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
+console.log(bmi(80,1.8));
