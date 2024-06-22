c6a09e1ef6f4deee037e7177911e10fcffa0e72e
Faiem-qy <f.quanyoung@gmail.com>
2024-06-21 21:55:52 -0400

    practice

--- cw.js
+++ cw.js
@@ -3984,3 +3984,25 @@ function hoopCount(n) {
       return "Keep at it until you get it";
   }
 }
+
+
+function whatday(num) { 
+  switch(num) {
+      case 1:
+          return "Sunday";
+      case 2:
+          return "Monday";
+      case 3:
+          return "Tuesday";
+      case 4:
+          return "Wednesday";
+      case 5:
+          return "Thursday";
+      case 6:
+          return "Friday";
+      case 7:
+          return "Saturday";
+      default:
+          return "Wrong, please enter a number between 1 and 7";
+  }
+}
