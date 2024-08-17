d5816a34d6739d73364b7f5ed6b8fb5d36fb31d0
Faiem-qy <f.quanyoung@gmail.com>
2024-08-16 22:52:35 -0400

    fix bug

--- cw.js
+++ cw.js
@@ -4065,7 +4065,7 @@ function remainder(n, m) {
 
 function findNeedle(haystack) {
   const position = haystack.indexOf("needle");
-  return "found the needle at positipn" + position;
+  return "found the needle at position " + position; 
 }
 
 console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]));
