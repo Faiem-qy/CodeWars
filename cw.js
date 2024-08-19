ce2eb6e956123a0730eef7596e3741bc71a8e101
Faiem-qy <f.quanyoung@gmail.com>
2024-08-19 19:05:16 -0400

    practice

--- cw.js
+++ cw.js
@@ -4108,3 +4108,13 @@ function sumOfDifferences(arr) {
   
   return sum;
 }
+
+
+function abbrevName(name) {
+  let words = name.split(' ');
+  
+  return words[0][0].toUpperCase() + '.' + words[1][0].toUpperCase();
+}
+
+console.log(abbrevName("Sam Harris"));  // Output: S.H
+console.log(abbrevName("patrick feeney"));  // Output: P.F
