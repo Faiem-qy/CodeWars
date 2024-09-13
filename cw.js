a545d2974cfbceccc92e4021e408ecd650f65cab
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-09-12 20:47:23 -0400

    practice

--- cw.js
+++ cw.js
@@ -4346,3 +4346,14 @@ function abbrevName(name) {
 
     return initials.join('.');
 }
+
+const maxSequence = function(arr){
+ let maxSum = 0
+ let currentSum = 0;
+  
+  for (let num of arr) {
+    currentSum = Math.max(0, currentSum + num);
+    maxSum = Math.max(maxSum, currentSum)
+  }
+  return maxSum
+}
