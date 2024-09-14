e9a5bbf271e79fa1424665f2b74fbd8be3a4bb3f
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-09-14 18:10:08 -0400

    practice

--- cw.js
+++ cw.js
@@ -4362,3 +4362,8 @@ function sumTwoSmallestNumbers(numbers) {
   numbers.sort((a, b) => a - b);
   return numbers[0] + numbers[1];
 }
+
+
+function toBinary(n) {
+  return parseInt(n.toString(2));
+}
