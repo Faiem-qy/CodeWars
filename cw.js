4ccf18bf9a1f08f61196a1172ceb54e28ce32d17
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-09-15 10:43:06 -0400

    practice

--- cw.js
+++ cw.js
@@ -4367,3 +4367,17 @@ function sumTwoSmallestNumbers(numbers) {
 function toBinary(n) {
   return parseInt(n.toString(2));
 }
+
+function expressionMatter(a, b, c) {
+  
+  const result1 = a + b + c;
+  const result2 = a * b * c;
+  const result3 = (a + b) * c;
+  const result4 = a * (b + c);
+  const result5 = (a * b) + c;
+  const result6 = a + (b * c);
+
+  const maxResult = Math.max(result1, result2, result3, result4, result5, result6);
+
+  return maxResult;
+}
