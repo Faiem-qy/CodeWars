eebe03b50bdd600d46b19cbb6006a1a0600cb886
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-09-21 08:20:21 -0400

    practice

--- cw.js
+++ cw.js
@@ -4437,3 +4437,27 @@ function evenNumbers(array, number) {
   const evenNums = array.filter(num => num % 2 === 0);
   return evenNums.slice(-number);
 }
+
+function add(a, b) {
+    return a + b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function mod(a, b) {
+    return a % b;
+}
+
+function exponent(a, b) {
+    return Math.pow(a, b);
+}
+
+function subt(a, b) {
+    return a - b;
+}
