e31c98f63b53777148f65fa3330ed54c1f0d82b6
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-10-04 19:02:50 -0400

    practice

--- cw.js
+++ cw.js
@@ -4606,3 +4606,31 @@ function pillars(numPill, dist, width) {
 }
 
 console.log(pillars(1, 20, 25))
+
+
+function howManydays(month) {
+  var days;
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      days = 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      days = 30;
+      break;
+    case 2:
+      days = 28;
+      break;
+    default:
+      days = 0; // This case should never be reached since month is always between 1 and 12
+  }
+  return days;
+}
