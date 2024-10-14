818a63976012b94066c4ad1031eaee7f1548e844
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-10-14 16:28:52 -0400

    practice

--- cw.js
+++ cw.js
@@ -4708,3 +4708,19 @@ function remove(string) {
   return string.replace(/!/g, '') + '!';
 }
 console.log(remove("Hi!!!"));
+
+function defineSuit(card) {
+  const suit = card.slice(-1);
+  switch (suit) {
+    case '♣':
+      return 'clubs';
+    case '♦':
+      return 'diamonds';
+    case '♥':
+      return 'hearts';
+    case '♠':
+      return 'spades';
+    default:
+      return 'unknown suit';
+  }
+}
