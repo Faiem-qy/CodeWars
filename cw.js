962b400325f0ea909d6932c2b1c1453c5c87d07d
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-12-09 21:06:40 -0500

    Update cw.js

--- cw.js
+++ cw.js
@@ -5399,3 +5399,21 @@ const sequenceSum = (begin, end, step) => {
 const isSquare = function(n) {
   return n >= 0 && Number.isInteger(Math.sqrt(n));
 }
+
+const humanYearsCatYearsDogYears = function(humanYears) {
+  let catYears = 0;
+  let dogYears = 0;
+
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 24;
+    dogYears = 24;
+  } else if (humanYears > 2) {
+    catYears = 24 + (humanYears - 2) * 4;
+    dogYears = 24 + (humanYears - 2) * 5;
+  }
+
+  return [humanYears, catYears, dogYears];
+};
