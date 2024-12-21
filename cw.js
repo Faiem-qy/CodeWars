e693ffe83b6a4251175f1c75a26ef9d607101ed5
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-12-21 10:06:53 -0500

    Update cw.js

--- cw.js
+++ cw.js
@@ -5596,3 +5596,17 @@ console.log(greet("czech"));    // "Vitejte"
 console.log(greet("japanese")); // "Welcome" (default)
 console.log(greet("german"));   // "Willkommen"
 console.log(greet(""));         // "Welcome" (default)
+
+function roundToNext5(n) {
+  return Math.ceil(n / 5) * 5;
+}
+
+
+console.log(roundToNext5(0));   // 0
+console.log(roundToNext5(2));   // 5
+console.log(roundToNext5(3));   // 5
+console.log(roundToNext5(12));  // 15
+console.log(roundToNext5(21));  // 25
+console.log(roundToNext5(30));  // 30
+console.log(roundToNext5(-2));  // 0
+console.log(roundToNext5(-5));  // -5
