d48815822e0e3f99db0dd11e0e59bcb646c0f06c
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2024-12-24 08:39:48 -0500

    Update cw.js

--- cw.js
+++ cw.js
@@ -5632,3 +5632,12 @@ function sayHello(name, city, state) {
 
 
 console.log(sayHello(['John', 'Doe'], 'Los Angeles', 'California')); // "Hello, John Doe! Welcome to Los Angeles, California!"
+
+function filter_list(l) {
+  return l.filter(item => typeof item !== 'string');
+}
+
+
+console.log(filter_list([1, 2, 'a', 'b']));
+console.log(filter_list([1, 'a', 'b', 0, 15]));
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123]));
